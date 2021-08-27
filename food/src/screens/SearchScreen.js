75151f62a4aa03c7dbd70cadbd870531359395d1
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-08-27 11:05:40 +0300

    add food folder

---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import ResultsList from '../components/ResultsList';
+import SearchBar from '../components/SerchBar';
+import useResults from '../hooks/useResults';
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
+
+  return (
+    <>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective"
+        />
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice('$$$')}
+          title="Big Spender"
+        />
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
